e7bc5591561d0399387b8eb71af5e1a9ace61281
William <willspri@gmail.com>
2025-05-21 14:35:55 +1200

    started

---
+++ GameView.cs
@@ -0,0 +1,36 @@
+﻿using Android.Content;
+using Android.Graphics;
+using Android.Views;
+
+namespace FlappyBird
+{
+    public class GameView : View
+    {
+        private Paint paint = new Paint();
+        private float birdY = 500;
+        private float gravity = 3;
+        private float velocity = 0;
+
+        public GameView(Context context) : base(context)
+        {
+        }
+
+        protected override void OnDraw(Canvas canvas)
+        {
+            base.OnDraw(canvas);
+
+            velocity += gravity;
+            birdY += velocity;
+
+            paint.Color = Color.Yellow;
+            canvas.DrawCircle(200, birdY, 50, paint);
+
+            Invalidate(); // Redraw continuously
+        }
+
+        public void Flap()
+        {
+            velocity = -30;
+        }
+    }
+}
---
+++ MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Views;
+
+namespace FlappyBird
+{
+    [Activity(Label = "FlappyBird", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            var gameView = new GameView(this);
+            SetContentView(gameView);
+
+            gameView.Touch += (s, e) =>
+            {
+                if (e.Event.Action == MotionEventActions.Down)
+                {
+                    gameView.Flap();
+                }
+            };
+        }
+    }
+}
